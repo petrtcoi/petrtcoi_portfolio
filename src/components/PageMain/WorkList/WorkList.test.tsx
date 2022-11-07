06dae0abcae5e671d88724566d1fba0c80811115
Petr Tcoi <petrtcoi@outlook.com>
2022-11-08 02:19:58 +0300

    rework tests

--- src/components/PageMain/WorkList/WorkList.test.tsx
+++ src/components/PageMain/WorkList/WorkList.test.tsx
@@ -5,24 +5,33 @@ import { works } from '../../../__tests__/data/works'
 
 describe('WorkList Section', () => {
 
-    describe("WORKLIST", () => {
-        test('Выводится', async () => {
-            render(<WorkList works={ [] } />)
-            expect(screen.getByRole('list', {name: /список работ/i})).toBeVisible()
-        })
-        test('Выводит заголовок <h2> "Список работ"', async () => {
-            render(<WorkList works={ [] } />)
-            expect(screen.getByRole('heading', {level: 2, name: /список работ/i})).toBeVisible()
-        })
-        test('display correct qnty of works', async () => {
-            render(<WorkList works={ works } />)
-            expect(screen.getAllByRole('listitem', {name: /работа/i}).length).toBe(works.length)
-        })
-        test('Display all work titles', async () => {
-            render(<WorkList works={ works } />)
-            for (const work of works) {
-                expect(screen.getByRole('heading', {name: work.title})).toBeVisible()
-            }
-        })
+  const getWorkList = () => screen.getByRole('list', { name: /список работ/i })
+  const getAllWorks = () => screen.getAllByRole('listitem', { name: /работа/i })
+  const getWorkTitleByTitle = (title: string) => screen.getByRole('heading', { name: title })
+
+
+  describe("WORKLIST", () => {
+
+    test('Выводится', async () => {
+      render(<WorkList works={ [] } />)
+      expect(getWorkList()).toBeVisible()
     })
+
+    test('Выводит заголовок <h2> "Список работ"', async () => {
+      render(<WorkList works={ [] } />)
+      expect(getWorkList()).toBeVisible()
+    })
+
+    test('display correct qnty of works', async () => {
+      render(<WorkList works={ works } />)
+      expect(getAllWorks().length).toBe(works.length)
+    })
+
+    test('Display all work titles', async () => {
+      render(<WorkList works={ works } />)
+      for (const work of works) {
+        expect(getWorkTitleByTitle(work.title)).toBeVisible()
+      }
+    })
+  })
 })
