8110f2c557eba6a4eec45771d9dfb813494d252d
Petr Tcoi <petrtcoi@outlook.com>
2022-11-08 22:16:19 +0300

    fix testes

--- playwright.config.ts
+++ playwright.config.ts
@@ -66,18 +66,8 @@ const config: PlaywrightTestConfig = {
     },
 
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
-    },
-    {
-      name: 'Mobile Safari',
-      use: {
-        ...devices['iPhone 12'],
-      },
-    },
+    
+    
 
     /* Test against branded browsers. */
     // {
