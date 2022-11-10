7d092b5082511a71ef819105d71916be9ff272c6
Petr Tcoi <petrtcoi@outlook.com>
2022-11-10 12:59:15 +0300

    small refactoring

--- src/components/PageMain/GeneralInfo/AboutText/AboutText.tsx
+++ src/components/PageMain/GeneralInfo/AboutText/AboutText.tsx
@@ -6,30 +6,32 @@ type AboutTextProps = {}
 
 const AboutText: React.FC<AboutTextProps> = (_props) => {
 
-    return (
-        <div>
-            <p className="text--large accent" style={ { marginTop: "0px" } }>
-                Привет! Меня зовут Петр. Я создал этот сайт в качестве своего портфолио. Здесь буду выкладывать &quot;лабораторные&quot; работы и ссылки на их описание.
-            </p>
-            <p>
-                Пишу сайты на <span className="text--accent">React</span> с 2017 года. Начинал как хобби, но впоследствии прокачался и нашел примение своим умениям в коммерческой области: интернет-магазины, написанные на <span className="text--accent">NextJS</span> (например, <Link href="https://arboniashop.ru" target="_blank" rel="noreferrer">arboniashop.ru</Link>). Создана также рабочая панель для подготовки коммерческих предложений покупателям, интегрированная с прайс-листами, загруженными в Google Docs, онлайн-кассой, складской программой, рабочим WhatsApp (Twilio).
-            </p>
-            <p>
-                Мой основной стек: React / Next, Express, MongoDb, Firebase Realtime. Был хобби-проект на React Native. Также могу написать простые скрипты на Python (использовал для автоматической выгрузки остатков, мониторинга цен и т.п.).
-            </p>
-            <p>
-               Сайт доступен на&nbsp;
-                <Link
-                    href="https://github.com/petrtcoi/petrtcoi_portfolio"
-                    target="_blank"
-                    className="accent"
-                >
-                    Guthub
-                </Link>
-                . Буду признателен за любую обратную связь.
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <p className="text--large accent" style={ { marginTop: "0px" } }>
+        Привет! Меня зовут Петр. Я создал этот сайт в качестве своего портфолио. Здесь буду выкладывать &quot;лабораторные&quot; работы и ссылки на их описание.
+      </p>
+      <p>
+        Пишу сайты на <span className="text--accent">React</span> с 2017 года. Начинал как хобби, но впоследствии прокачался и нашел примение своим умениям в коммерческой области: интернет-магазины, написанные на <span className="text--accent">NextJS</span> (например, <Link href="https://arboniashop.ru" target="_blank" rel="noreferrer">arboniashop.ru</Link>). Создана также рабочая панель для подготовки коммерческих предложений покупателям, интегрированная с прайс-листами, загруженными в Google Docs, онлайн-кассой, складской программой, рабочим WhatsApp (Twilio).
+      </p>
+      <p>
+        Мой основной стек: React / Next, Express, MongoDb, Firebase Realtime. Был хобби-проект на React Native. Также могу написать простые скрипты на Python (использовал для автоматической выгрузки остатков, мониторинга цен и т.п.).
+      </p>
+      <p>
+        Сайт доступен на&nbsp;
+        <span>
+          <Link
+            href="https://github.com/petrtcoi/petrtcoi_portfolio"
+            target="_blank"
+            className="accent"
+          >
+            Guthub
+          </Link>
+        </span>
+        . Буду признателен за любую обратную связь.
+      </p>
+    </div>
+  )
 }
 
 export default AboutText
--- src/components/PageMain/WorkList/WorkSingle/WorkSingle.test.tsx
+++ src/components/PageMain/WorkList/WorkSingle/WorkSingle.test.tsx
@@ -1,65 +1,63 @@
 import { render, screen } from '@testing-library/react'
 import WorkSingle from './WorkSingle'
-
 import { WorkLinkLabels } from '../../../../types/work.type'
-
 import { works } from '../../../../__tests__/data/works'
-import { linkSync } from 'fs'
 
 
 
 describe('Work Single', () => {
 
 
-    const getWorkByTitle = (title: string) => screen.getByRole("heading", { name: title })
-    const getDescription = () => screen.getByLabelText(/описание работы/i)
-    const getTagsList = () => screen.getByLabelText(/используемые технологии/i)
+  const getWorkByTitle = (title: string) => screen.getByRole("heading", { name: title })
+  const getDescription = () => screen.getByLabelText(/описание работы/i)
+  const getTagsList = () => screen.getByLabelText(/используемые технологии/i)
 
-    const workOne = works[0]
-    beforeAll(() => {
-        if (workOne.links.devto === undefined ||
-            workOne.links.vcru === undefined ||
-            workOne.links.local === undefined
-        ) {
-            throw new Error('Не правильно выбран для теста пример WORK. Нужны все ссылки на страницы')
-        }
-    })
-    
+  const workOne = works[0]
+  beforeAll(() => {
+    if (
+      workOne.links.devto === undefined ||
+      workOne.links.vcru === undefined ||
+      workOne.links.local === undefined
+    ) {
+      throw new Error('Не правильно выбран для теста пример WORK. Нужны все ссылки на страницы')
+    }
+  })
 
-    test('Загружается заголовок работы', async () => {
-        render(<WorkSingle work={ workOne } />)
-        expect(getWorkByTitle(workOne.title)).toBeVisible()
-    })
 
-    test('Выводится описание работы', async () => {
-        render(<WorkSingle work={ workOne } />)
-        expect(getDescription()).toHaveTextContent(workOne.description)
-    })
+  test('Загружается заголовок работы', async () => {
+    render(<WorkSingle work={ workOne } />)
+    expect(getWorkByTitle(workOne.title)).toBeVisible()
+  })
 
-    test('Теги работы выводятся через запятую', async () => {
-        render(<WorkSingle work={ workOne } />)
-        expect(getTagsList()).toHaveTextContent(workOne.tags.join(', '))
-    })
+  test('Выводится описание работы', async () => {
+    render(<WorkSingle work={ workOne } />)
+    expect(getDescription()).toHaveTextContent(workOne.description)
+  })
 
-    test('Выводит все ссылки на страницы', () => {
-        render(<WorkSingle work={ workOne } />)
-        for (const label of Object.values(WorkLinkLabels)) {
-            expect(screen.getByText(label))
-        }
-    })
+  test('Теги работы выводятся через запятую', async () => {
+    render(<WorkSingle work={ workOne } />)
+    expect(getTagsList()).toHaveTextContent(workOne.tags.join(', '))
+  })
 
-    test('Не выводит ссылки на drvto, если их нет в работе', () => {
-        render(<WorkSingle work={ { ...workOne, links: { ...workOne.links, devto: undefined } } } />)
-        expect(screen.queryByText(WorkLinkLabels.devto)).not.toBeInTheDocument()
-    })
-    test('Не выводит ссылки на vcru, если их нет в работе', () => {
-        render(<WorkSingle work={ { ...workOne, links: { ...workOne.links, vcru: undefined } } } />)
-        expect(screen.queryByText(WorkLinkLabels.vcru)).not.toBeInTheDocument()
-    })
-    test('Не выводит ссылки на локальную, если их нет в работе', () => {
-        render(<WorkSingle work={ { ...workOne, links: { ...workOne.links, local: undefined } } } />)
-        expect(screen.queryByText(WorkLinkLabels.local)).not.toBeInTheDocument()
-    })
+  test('Выводит все ссылки на страницы', () => {
+    render(<WorkSingle work={ workOne } />)
+    for (const label of Object.values(WorkLinkLabels)) {
+      expect(screen.getByText(label))
+    }
+  })
+
+  test('Не выводит ссылки на drvto, если их нет в работе', () => {
+    render(<WorkSingle work={ { ...workOne, links: { ...workOne.links, devto: undefined } } } />)
+    expect(screen.queryByText(WorkLinkLabels.devto)).not.toBeInTheDocument()
+  })
+  test('Не выводит ссылки на vcru, если их нет в работе', () => {
+    render(<WorkSingle work={ { ...workOne, links: { ...workOne.links, vcru: undefined } } } />)
+    expect(screen.queryByText(WorkLinkLabels.vcru)).not.toBeInTheDocument()
+  })
+  test('Не выводит ссылки на локальную, если их нет в работе', () => {
+    render(<WorkSingle work={ { ...workOne, links: { ...workOne.links, local: undefined } } } />)
+    expect(screen.queryByText(WorkLinkLabels.local)).not.toBeInTheDocument()
+  })
 
 
 
