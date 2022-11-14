8df50445b8f0df256275169f7185b253c7689758
Petr Tcoi <petrtcoi@outlook.com>
2022-11-14 11:55:20 +0300

    add TagManager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
-import type { AppProps } from 'next/app'
 import Layout from '../src/components/Layout/Layout'
+import TagManager from 'react-gtm-module'
+import type { AppProps } from 'next/app'
+
 
 import '../src/assets/styles/_styles.css'
+import React from 'react'
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  
+  React.useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-Z972NPKMWH' });
+}, []);
   return (
     <Layout>
       <Component { ...pageProps } />
