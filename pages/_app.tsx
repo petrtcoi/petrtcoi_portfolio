1ebde62db17fcfb1814a14df2a34398896ba5d19
Petr Tcoi <petrtcoi@outlook.com>
2022-11-14 12:40:10 +0300

    remove gtag add Google Tag??

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import Layout from '../src/components/Layout/Layout'
-import TagManager from 'react-gtm-module'
 import type { AppProps } from 'next/app'
 
 
@@ -9,9 +8,6 @@ import React from 'react'
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  React.useEffect(() => {
-    TagManager.initialize({ gtmId: 'G-Z972NPKMWH' });
-}, []);
   return (
     <Layout>
       <Component { ...pageProps } />
--- src/components/Layout/HeadMetaTags/HeadMetaTags.tsx
+++ src/components/Layout/HeadMetaTags/HeadMetaTags.tsx
@@ -1,25 +1,41 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import React from 'react'
 
 type HeadMetaProps = {}
 
 const HeadMeta: React.FC<HeadMetaProps> = (_props) => {
     return (
-        <Head>
-            {/* Общее */ }
-            <meta charSet="utf-8" />
-            <title>React разработка. Портфолио Petr Tcoi</title>
-            <meta name="description" content="Сайт-портфолио от Petr Tcoi. Работаю с ReactJS / NextJS. Открыт к предложениям." />
-            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <>
+            <Head>
+                {/* Общее */ }
+                <meta charSet="utf-8" />
+                <title>React разработка. Портфолио Petr Tcoi</title>
+                <meta name="description" content="Сайт-портфолио от Petr Tcoi. Работаю с ReactJS / NextJS. Открыт к предложениям." />
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 
-            {/* Favicons */ }
-            <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-            <meta name="msapplication-TileColor" content="#da532c" />
-            <meta name="theme-color" content="#ffffff"></meta>
-        </Head>
+                {/* Favicons */ }
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff"></meta>
+            </Head>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-Z972NPKMWH"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                { `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-Z972NPKMWH');
+                `}
+            </Script>
+        </>
     )
 }
 
