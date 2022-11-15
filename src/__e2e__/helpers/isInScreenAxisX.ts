f8710613eb59a271b4c90105258cb643f6dec251
Petr Tcoi <petrtcoi@outlook.com>
2022-11-16 02:10:22 +0300

    add Highlighted

---
+++ src/__e2e__/helpers/isInScreenAxisX.ts
@@ -0,0 +1,23 @@
+import { Locator, Page } from "@playwright/test"
+
+type Props = {
+  element: Locator,
+  page: Page
+}
+
+
+export const isInScreenAxisX = async (props: Props): Promise<boolean> => {
+  let isIn = false
+
+  const rect = await props.element.boundingBox()
+  const viewportSizes = props.page.viewportSize()
+  if (rect === null) throw new Error("element's boundingBox is null")
+  if (viewportSizes === null) throw new Error("page's viewportSize is null")
+
+  if (rect.x < viewportSizes.width && (rect.x + rect.width) > 0) {
+    isIn = true
+  }
+
+  return isIn
+
+}
