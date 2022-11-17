2416531e762a644070b9b139bd9fbecc5ba925d7
Petr Tcoi <petrtcoi@outlook.com>
2022-11-17 14:07:18 +0300

    google -> metrika

--- src/components/Layout/HeadMetaTags/HeadMetaTags.tsx
+++ src/components/Layout/HeadMetaTags/HeadMetaTags.tsx
@@ -23,17 +23,19 @@ const HeadMeta: React.FC<HeadMetaProps> = (_props) => {
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff"></meta>
             </Head>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=G-Z972NPKMWH`}
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="yandex-metrika" strategy="afterInteractive">
                 { `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){window.dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', 'G-Z972NPKMWH');
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                 
+                    ym(91236839, "init", {
+                         clickmap:true,
+                         trackLinks:true,
+                         accurateTrackBounce:true
+                    });
                 `}
             </Script>
         </>
