137af1de362601ab66a3700974c176cde60e33ff
Petr Tcoi <petrtcoi@outlook.com>
2022-12-12 13:09:05 +0300

    add json-server

--- src/__tests__/data/works.ts
+++ src/__tests__/data/works.ts
@@ -5,7 +5,7 @@ export const works: Work[] = [
         publishDate: '22 ноября 2021',
         title: 'Запуск сайта',
         description: 'Создание шаблона сайта с минимальным функционалом. Адаптивная верстка flexbox. Также используем CSS-переменные, в том числе и для эффективной смены темы сайта.',
-        tags: ['NextJS', 'Flexbox', 'CSS variables'],
+        keywords: ['NextJS', 'Flexbox', 'CSS variables'],
         links: {
             devto: 'http://example-1.com',
             vcru: 'https://example-1.com',
@@ -16,7 +16,7 @@ export const works: Work[] = [
         publishDate: '22 ноября 2021',
         title: 'Work 2',
         description: 'Work 3',
-        tags: ['tag1', 'tag3'],
+        keywords: ['tag1', 'tag3'],
         links: {
             devto: 'http://example-2.com',
             vcru: 'https://example-2.com',
@@ -27,7 +27,7 @@ export const works: Work[] = [
         publishDate: '22 ноября 2021',
         title: 'Work 3',
         description: 'Work 3',
-        tags: ['tag3'],
+        keywords: ['tag3'],
         links: {
             devto: 'http://example-3.com',
             vcru: 'https://example-3.com',
